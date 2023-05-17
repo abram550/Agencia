d9cd2a2437399a34a3a6244c4c3eed0f0e7b5c91
Abraham Josue Rojas Farelo <abrahamfaju18@gmail.com>
2023-05-17 16:43:12 -0500

    Mensaje de confirmación

--- Apps/Viajeros/admin.py
+++ Apps/Viajeros/admin.py
@@ -3,11 +3,13 @@ from Apps.Viajeros.models import Viajeros,ReferenciaFamiliar
 
 # Register your models here.
 
-
 class viajerosAdmin(admin.ModelAdmin):
-    pass
 
+    list_display = ('dni','nombreViajeros','direccionViajeros')
+    ordering = ('dni','nombreViajeros','direccionViajeros')
+    search_fields = ('dni','nombreViajeros','direccionViajeros')
+    list_filter = ('dni','nombreViajeros','direccionViajeros')
 
 
 admin.site.register(Viajeros, viajerosAdmin)
-admin.site.register(ReferenciaFamiliar, viajerosAdmin)
+admin.site.register(ReferenciaFamiliar)
