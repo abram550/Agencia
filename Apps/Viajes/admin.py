73a91d2ad8e7eb0d1b5dbeb3319c11dd3258e9fd
Abraham Josue Rojas Farelo <abrahamfaju18@gmail.com>
2023-05-17 16:55:07 -0500

    Mensaje de confirmación

--- Apps/Viajes/admin.py
+++ Apps/Viajes/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
-from Apps.Viajes.models import Viaje, Origen,Destino
+from Apps.Viajes.models import Viaje, Origen,Destino,MotivoViajes,ViajeMotivoViaje
 
 # Register your models here.
 
+class MembershipInline(admin.TabularInline):
+    model = ViajeMotivoViaje
+    extra = 1
 
 class viajesAdmin(admin.ModelAdmin):
+    inlines = (MembershipInline,)
+    #list_display = ('numero_plazas')
+
+class OrigenAdmin(admin.ModelAdmin):
     pass
+class DestinoAdmin(admin.ModelAdmin):
+    pass
+class MotivoViajesAdmin(admin.ModelAdmin):
+    pass
+    
 
 
 admin.site.register(Viaje, viajesAdmin)
-admin.site.register(Origen, viajesAdmin)
-admin.site.register(Destino, viajesAdmin)
+admin.site.register(Origen, OrigenAdmin)
+admin.site.register(Destino, DestinoAdmin)
+admin.site.register(MotivoViajes, MotivoViajesAdmin)
--- Apps/Viajes/models.py
+++ Apps/Viajes/models.py
@@ -2,42 +2,65 @@ from django.db import models
 from Apps.Viajeros.models import Viajeros
 from django.utils.timezone import now
 
+#-------------------------------
+class MotivoViajes(models.Model):
+    motivoviajes = models.CharField(max_length=100, verbose_name="Ingrese el motivo por lo caual Viaja")
 
+    def __str__(self):
+        return self.motivoviajes
 
+    class Meta:
+        verbose_name = "motivo Viajes"
+        verbose_name_plural = "motivo Viajes"
+
+#-------------------------------
 class Destino(models.Model):
-    nombre_destino = models.CharField(max_length=100, verbose_name = "Ingrese el Destino")
+    nombre_destino = models.CharField(max_length=100, verbose_name="Ingrese el Destino")
 
     def __str__(self):
         return self.nombre_destino
-    
+
     class Meta:
         verbose_name = "destino"
         verbose_name_plural = "destino"
-    
+
+#-------------------------------
 class Origen(models.Model):
-    nombre_origen = models.CharField(max_length=100, verbose_name = "Ingrese el Origen")
-    
+    nombre_origen = models.CharField(max_length=100, verbose_name="Ingrese el Origen")
+
     def __str__(self):
         return self.nombre_origen
-    
+
     class Meta:
         verbose_name = "origen"
         verbose_name_plural = "origen"
 
-
+#-------------------------------
 class Viaje(models.Model):
-    numero_plazas = models.CharField(max_length=10, verbose_name = "Ingrese el Numero plazas")
-    fecha_viaje = models.DateField(default=now,verbose_name = "Ingrese la fecha de viaje")
-    destino = models.ForeignKey(Destino,null=True,
-                                    blank=True, on_delete=models.CASCADE)
-    origen = models.ForeignKey(Origen,null=True,
-                                    blank=True, on_delete=models.CASCADE)
-    viajeros = models.ForeignKey(Viajeros,null=True,
-                                    blank=True, on_delete=models.CASCADE)
-    
+    numero_plazas = models.CharField(max_length=10, verbose_name="Ingrese el Numero plazas")
+    fecha_viaje = models.DateField(default=now, verbose_name="Ingrese la fecha de viaje")
+    destino = models.ForeignKey(Destino, null=True, blank=True, on_delete=models.CASCADE)
+    motivoViaje = models.ManyToManyField(MotivoViajes, through="ViajeMotivoViaje", verbose_name="ViajeMotivoViaje")
+    origen = models.ForeignKey(Origen, null=True, blank=True, on_delete=models.CASCADE)
+    viajeros = models.ForeignKey(Viajeros, null=True, blank=True, on_delete=models.CASCADE)
+
     def __str__(self):
         return f"{self.origen.nombre_origen} - {self.destino.nombre_destino}"
-    
+
+
     class Meta:
-            verbose_name = "Viaje"
-            verbose_name_plural = " Viaje"
+        verbose_name = "Viaje"
+        verbose_name_plural = " Viaje"
+
+#-------------------------------
+class ViajeMotivoViaje(models.Model):
+    viaje = models.ForeignKey(Viaje, null=True, blank=True, on_delete=models.CASCADE)
+    motivoViaje = models.ForeignKey(MotivoViajes, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.viaje} - {self.motivoViaje}"
+
+
+    class Meta:
+        verbose_name = "ViajeMotivoViaje"
+        verbose_name_plural = " ViajesMotivosViajes"
